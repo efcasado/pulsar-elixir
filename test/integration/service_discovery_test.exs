bc347ce6f2b0e2995693af9461560cd1b4e0f870
Giuseppe De Palma <dpl.gsp@tutanota.com>
2025-10-18 21:16:12 +0200

    refactor: use service discovery in consumer for topic lookup functionality

--- test/integration/service_discovery_test.exs
+++ test/integration/service_discovery_test.exs
@@ -32,14 +32,25 @@ defmodule Pulsar.Integration.ServiceDiscoveryTest do
     @tag telemetry_listen: [[:pulsar, :service_discovery, :lookup_topic, :stop]]
     test "topic lookup retrieves broker PID" do
       topic = @topic <> "-#{:erlang.unique_integer([:positive])}"
+      System.create_topic(topic)
 
-      {:ok, broker_pid} = ServiceDiscovery.lookup_topic(topic)
-
+      assert {:ok, broker_pid} = ServiceDiscovery.lookup_topic(topic)
       assert Process.alive?(broker_pid)
 
-      # Verify 1 lookup with success result
       stats = Utils.collect_lookup_stats()
       assert %{success_count: 1, failure_count: 0, total_count: 1} = stats
+    end
+
+    @tag telemetry_listen: [[:pulsar, :service_discovery, :lookup_topic, :stop]]
+    test "topic lookup fails for non-existing topics" do
+      result1 = ServiceDiscovery.lookup_topic("persistent://fake/fake/fake")
+      assert {:error, _reason} = result1
+
+      result2 = ServiceDiscovery.lookup_topic("persistent://public/fake/fake")
+      assert {:error, _reason} = result2
+
+      stats = Utils.collect_lookup_stats()
+      assert %{success_count: 0, failure_count: 2, total_count: 2} = stats
     end
   end
 end
