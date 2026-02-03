4ae3f2c8627607eac693fef994ccb18d8ab51778
Óscar de Arriba <oscar@dearriba.es>
2026-02-03 19:10:40 +0100

    Add tests

---
+++ test/integration/client/common_test.exs
@@ -0,0 +1,26 @@
+defmodule Pulsar.Integration.Client.CommonTest do
+  use ExUnit.Case, async: true
+
+  alias Pulsar.Test.Support.System
+
+  require Logger
+
+  @moduletag :integration
+
+  test "can spawn multiple clients at once" do
+    client_one = :common_client_1
+    client_two = :common_client_2
+
+    {:ok, _pid} =
+      Pulsar.start(
+        name: :supervisor_test_common,
+        clients: [
+          {client_one, [host: System.broker().service_url]},
+          {client_two, [host: System.broker().service_url]}
+        ]
+      )
+
+    assert Process.alive?(Process.whereis(client_one))
+    assert Process.alive?(Process.whereis(client_two))
+  end
+end
