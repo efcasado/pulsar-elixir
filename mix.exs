acd2b4cb97e81a6dfcf4c3f098adac342d10fd1c
Enrique Fernández <enrique@bluelabs.eu>
2026-08-02 19:40:27 +0200

    require Elixir 1.15+ due to supervisor auto_shutdown

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Pulsar.MixProject do
     [
       app: :pulsar,
       version: "2.11.1",
-      elixir: "~> 1.14",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
